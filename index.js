7479a9aed2952f42116eab8c79b65414bcbf7d97
Njenga Kariuki <njengak@me.com>
2019-01-10 00:58:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy Holidays!"
+  return `Happy Holidays!`
 }
 
 function happyHolidaysTo(name) {
-  return 'Happy holidays, ${name}!'
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidaysTo(holiday, name) {
